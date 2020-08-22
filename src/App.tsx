05e075a641496da57bfcd36a20bed2e5845b803a
Vitalii <vitaliyoliynykk@gmail.com>
2020-08-22 13:01:08 +0300

    redirect to default page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './App.scss';
 import Auth from './containers/Auth/Auth';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Notes from './containers/Notes/Notes';
 
 function App(): React.ReactElement {
@@ -11,6 +11,9 @@ function App(): React.ReactElement {
             <Switch>
                 <Route path="/auth" component={Auth} />
                 <Route path="/notes" component={Notes} />
+                <Route exact path="/">
+                    <Redirect to="/notes" />
+                </Route>
             </Switch>
         </Router>
     );
