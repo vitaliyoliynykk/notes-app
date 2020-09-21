22613b49d9876ca92ea44e7c455c6ffa42085de5
Nataliia <nat.leshchak@gmail.com>
2020-09-21 23:32:54 +0300

    fixed bugs

--- src/containers/Notes/Notes.tsx
+++ src/containers/Notes/Notes.tsx
@@ -60,10 +60,10 @@ class Notes extends React.Component<{}, NotesState> {
     }
 
     private updateNoteInLocalState(note: Note): Note[] {
+        note.date = Date.now();
         const stateNotes = this.state.notes;
         const indexToEdit = stateNotes.findIndex((stateNote) => stateNote.id === note.id);
         stateNotes[indexToEdit] = note;
-        note.date = Date.now();
         return stateNotes;
     }
 
@@ -76,13 +76,10 @@ class Notes extends React.Component<{}, NotesState> {
     private onNoteChange(note: Note): void {
         const updatedNotes = this.updateNoteInLocalState(note);
         const arraySortedByDate = updatedNotes.sort((a, b) => +b.date - +a.date);
-        console.log(arraySortedByDate);
-
         this.setState({
             ...this.state,
             notes: arraySortedByDate,
         });
-
         this.saveNoteToFirebase(note);
     }
 
