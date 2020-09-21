b173f6c0614c2b3263dd6fa72f9c4aec4af9a52f
Nataliia <nat.leshchak@gmail.com>
2020-09-21 15:11:29 +0300

    fixed current date and sorted it

--- src/containers/Notes/Notes.tsx
+++ src/containers/Notes/Notes.tsx
@@ -63,6 +63,7 @@ class Notes extends React.Component<{}, NotesState> {
         const stateNotes = this.state.notes;
         const indexToEdit = stateNotes.findIndex((stateNote) => stateNote.id === note.id);
         stateNotes[indexToEdit] = note;
+        note.date = Date.now();
         return stateNotes;
     }
 
@@ -74,7 +75,14 @@ class Notes extends React.Component<{}, NotesState> {
 
     private onNoteChange(note: Note): void {
         const updatedNotes = this.updateNoteInLocalState(note);
-        this.setState({ ...this.state, notes: updatedNotes });
+        const arraySortedByDate = updatedNotes.sort((a, b) => +b.date - +a.date);
+        console.log(arraySortedByDate);
+
+        this.setState({
+            ...this.state,
+            notes: arraySortedByDate,
+        });
+
         this.saveNoteToFirebase(note);
     }
 
